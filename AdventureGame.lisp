fe9f191a151537c8a4603f1e79a9eaba38765a34
tamac <tamac@demodara.local>
2018-04-18 23:36:48 +0900

    F 情景を描写する関数を追加

--- AdventureGame.lisp
+++ AdventureGame.lisp
@@ -5,3 +5,8 @@
                                      there is a well in front of you.))
                         (attic (you are in the attic.
                                     there is a giant welding torch in the corner.))))
+
+
+(defun describe-location (location nodes)
+  "情景を描写する"
+  (cadr (assoc location nodes)))
