37a4a7ae792d395ee46b6b4a904db07d7d842c84
tamac <tamac@demodara.local>
2018-04-29 02:04:36 +0900

    R ノード情報とエッジ情報の変数名を修正

--- AdventureGame.lisp
+++ AdventureGame.lisp
@@ -1,5 +1,5 @@
 ; ゲーム中の場所の描写
-(defparameter *nodes* '((living-room (you are in the living-room.
+(defparameter *wizard-nodes* '((living-room (you are in the living-room.
                                           a wizard is snoring loudly on the couch.))
                         (garden (you are in a beatiful garden.
                                      there is a well in front of you.))
@@ -7,7 +7,7 @@
                                     there is a giant welding torch in the corner.))))
 
 ; ゲームのマップ上でプレーヤーが場所間を移動可能な通り道
-(defparameter *edges* '((living-room (garden west door)
+(defparameter *wizard-edges* '((living-room (garden west door)
                                      (attic upstairs ladder))
                         (garden (living-room east door))
                         (attic (living-room downstairs ladder))))
@@ -68,14 +68,14 @@
 ;; dirtyな関数群
 (defun look ()
   "全てを描写する"
-  (append (describe-location *location* *nodes*)
-          (describe-paths *location* *edges*)
+  (append (describe-location *location* *wizard-nodes*)
+          (describe-paths *location* *wizard-edges*)
           (describe-objects *location* *objects* *object-locations*)))
 
 (defun walk (direction)
   "ゲーム世界を動き回る"
   (let ((next (find direction
-                    (cdr (assoc *location* *edges*))
+                    (cdr (assoc *location* *wizard-edges*))
                     :key #'cadr)))
     (if next
         (progn (setf *location* (car next))
