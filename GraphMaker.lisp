5aaa61a2a2d8e7a151ee9dd9af8a47a7ef173f61
tamac <tamac@demodara.local>
2018-04-30 01:10:35 +0900

    R ドキュメントコメントを詳細に記載

--- GraphMaker.lisp
+++ GraphMaker.lisp
@@ -1,3 +1,6 @@
 (defun dot-name (exp)
-  "ノードの識別子を変換する"
+  "ノードの識別子を変換する
+   exp: symbol
+    'living-room > LIVING_ROOM
+    'foo! > FOO_"
   (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
