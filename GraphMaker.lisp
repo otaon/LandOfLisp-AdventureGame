23d1b62646d66c62f9108d5e10e270f242a3ee7d
tamac <tamac@demodara.local>
2018-04-30 01:11:39 +0900

    F グラフのノードにラベルをつける関数を追加

--- GraphMaker.lisp
+++ GraphMaker.lisp
@@ -4,3 +4,18 @@
     'living-room > LIVING_ROOM
     'foo! > FOO_"
   (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
+
+; ラベル名の最大文字数
+(defparameter *max-label-length* 30)
+
+(defun dot-label (exp)
+  "グラフのノードにラベルをつける
+   exp: symbol
+    *max-label-length*より長い文字列 > 文末に...がついた文字列"
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+        (if (> (length s) *max-label-length*)
+            (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+            s))
+      ""))
+
