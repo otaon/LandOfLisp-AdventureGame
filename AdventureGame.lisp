23cb208c81cd72fbeadf89e2e4f4f10c2a4cfbdd
tamac <tamac@demodara.local>
2018-05-01 17:06:47 +0900

    R インデントを修正

--- AdventureGame.lisp
+++ AdventureGame.lisp
@@ -1,16 +1,16 @@
 ; ゲーム中の場所の描写
 (defparameter *wizard-nodes* '((living-room (you are in the living-room.
-                                          a wizard is snoring loudly on the couch.))
-                        (garden (you are in a beatiful garden.
-                                     there is a well in front of you.))
-                        (attic (you are in the attic.
-                                    there is a giant welding torch in the corner.))))
+                                                 a wizard is snoring loudly on the couch.))
+                               (garden (you are in a beatiful garden.
+                                            there is a well in front of you.))
+                               (attic (you are in the attic.
+                                           there is a giant welding torch in the corner.))))
 
 ; ゲームのマップ上でプレーヤーが場所間を移動可能な通り道
 (defparameter *wizard-edges* '((living-room (garden west door)
-                                     (attic upstairs ladder))
-                        (garden (living-room east door))
-                        (attic (living-room downstairs ladder))))
+                                            (attic upstairs ladder))
+                               (garden (living-room east door))
+                               (attic (living-room downstairs ladder))))
 
 ; ゲーム世界に存在するオブジェクトのリスト
 (defparameter *objects*  '(whiskey bucket frog chain))
